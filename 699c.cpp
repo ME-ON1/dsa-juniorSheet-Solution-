b520a680f75fde9ac15667551e8099a05db58727
Tarun Sharma <starun.1998@gmail.com>
2021-04-18 14:27:05 +0530

    corrected problem

--- 699c.cpp
+++ 699c.cpp
@@ -1,23 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std ;
-
+#define INF 1e9
 #define ll long long
 
 int main ( ){
 	int n ;
 	cin >> n;
+	vector<vector<int>> dp(n + 1 , vector<int> (3,INF)) ;
 	vector<int> ar(n) ;
-	for(int i = 0 ; i< n ; i++) {
+
+	for(int i = 0 ; i < n ; i++)
 		cin >> ar[i] ;
-	}
-	int restDays = 0 ;
-	for(int i = 0 ; i < n -1;i++ ) {
-		if(!ar[i]) restDays++ ;
 
-		else if(ar[i] == ar[i+1] && ar[i+1 ] != 3 ) restDays++ ;
-
-		else if(ar[i] != ar[i+1] && (ar[i] == 3 || ar[i+1] == 3)) restDays++ ;
+	dp[0][0] = 1 ;
+	if(ar[0] == 1 ){
+		dp[0][1] = 0 ;
 	}
-	cout << restDays ;
-    return 0;
+	if(ar[0] == 2) {
+		dp[0][2] = 0 ;
+	}
+	if(ar[0] == 3 ){
+		dp[0][2] = dp[0][1] = 0 ;
+	}
+
+	for(int i = 1 ; i < n ; i++ ) {
+		dp[i][0] = 1 + min(dp[i-1][0], min(dp[i-1][1], dp[i-1][2])) ;
+
+		if(ar[i] == 1 || ar[i] == 3) { //contest
+			dp[i][1] = min(dp[i-1][0], dp[i-1][2]) ;
+		}
+		if(ar[i] == 2 || ar[i] == 3){
+			dp[i][2] = min(dp[i-1][0], dp[i-1][1]);
+		}
+	}
+	//for(int i = 0 ; i < n ; i++ ) {
+		//for(int j = 0 ; j < 3 ; j++ ) {
+			//cout << dp[i][j] << " " ;
+		//}
+		//cout << endl ;
+	//}
+
+	cout << min(dp[n -1][0], min(dp[n-1][1], dp[n-1][2])) << endl ;
+	return 0;
 }
